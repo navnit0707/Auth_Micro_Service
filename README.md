0e45e7ac9d5cf06134d8e42a93f8878cb0a17b31
navnit0707 <knavnit81@gmail.com>
2023-07-31 13:10:23 +0530

    prepared user model

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 
 ### command
 
+- npx sequelize init
 - npx sequelize db:create
+- npx sequelize model:generate --name User --attributes email:string,password:string
+-
---
+++ src/migrations/20230731072344-create-user.js
@@ -0,0 +1,43 @@
+"use strict";
+
+const { sequelize } = require("../models");
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Users", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        validate: {
+          isEmail: true,
+        },
+      },
+      email: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.STRING,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+          len: [2, 100],
+        },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Users");
+  },
+};
