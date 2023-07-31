4eb8e3e9b8c63ac8bf3abc891b4812627e5c0732
navnit0707 <knavnit81@gmail.com>
2023-07-31 14:12:00 +0530

    added signup model,respo,service,routes, and password encrypt

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,7 @@
-"use strict";
 const { Model } = require("sequelize");
+const bcrypt = require("bcrypt");
+
+const { SALT } = require("../config/serverConfig");
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -25,7 +27,7 @@ module.exports = (sequelize, DataTypes) => {
         type: DataTypes.STRING,
         allowNull: false,
         validate: {
-          len: [2, 100],
+          len: [3, 100],
         },
       },
     },
@@ -34,5 +36,10 @@ module.exports = (sequelize, DataTypes) => {
       modelName: "User",
     }
   );
+
+  User.beforeCreate((user) => {
+    const encryptedPassword = bcrypt.hashSync(user.password, SALT);
+    user.password = encryptedPassword;
+  });
   return User;
 };
